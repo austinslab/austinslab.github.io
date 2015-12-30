6ecc39c0666ef218dd66b7dde28a4eaf09f8d115
Austin <aglensmith@gmail.com>
2015-12-29 21:27:19 -0600

    added zoom feature to concepts, removed table and added responsiveness to about page. Removed border and background from illustration thumbs

--- images.js
+++ images.js
@@ -37,39 +37,42 @@ function addIllustrations (list) {
 
 
 //ADD CONCEPTS
-function addConcept (image) {
+function addConcept (image, img_num) {
         
-    var src = image[0];
-    var title = image[1];
+  var src = image[0];
+  var title = image[1];
+  var box = document.getElementById("thumb-box");
+  var div = document.createElement("div");
+  var thumbdiv = document.createElement("div");
+  var a = document.createElement("a");
+  var img = document.createElement("img");
+  var titlediv = document.createElement("div");
+  var untarget = document.createElement("a")
 
-    var box = document.getElementById("thumbBox2");
-    var div = document.createElement("div");
-    var thumbdiv = document.createElement("div");
-    var a = document.createElement("a");
-    var img = document.createElement("img");
-    var titlediv = document.createElement("div");
+  div.setAttribute("class", "col-xs-12 col-md-6 col-lg-4 img");
+  div.setAttribute("id", "img" + img_num);
+  thumbdiv.setAttribute("class", "thumbnail");
+  a.setAttribute("href", "#img" + img_num);
+  a.setAttribute("class", "thumbnail");
+  img.setAttribute("src", src);
+  img.setAttribute("title", title);
+  titlediv.setAttribute("class", "caption text-center h3");
+  untarget.setAttribute("href", "#imgs");
+  untarget.setAttribute("class", "thumbnail untarget");
 
-    div.setAttribute("class", "col-xs-12 col-md-3 col-md-offset-2");
-    thumbdiv.setAttribute("class", "");
-    a.setAttribute("href", src);
-    a.setAttribute("class", "thumbnail");
-    img.setAttribute("src", src);
-    img.setAttribute("title", title);
-    titlediv.setAttribute("class", "caption text-center h3");
+  box.appendChild(div);
+  div.appendChild(a); 
+  a.appendChild(img);
+  div.appendChild(untarget);
 
-    box.appendChild(div);
-    div.appendChild(thumbdiv);
-    thumbdiv.appendChild(a);
-    thumbdiv.appendChild(titlediv);
-    a.appendChild(img);
-
-    titlediv.appendChild(document.createTextNode(title));
+  titlediv.appendChild(document.createTextNode(title));
 }
     
 function addConcepts (list) {
         
     for (var i = 0; i < list.length; i++) {
-        addConcept(list[i]);
+        addConcept(list[i], i);
+        console.log(i);
         }
 }
 
